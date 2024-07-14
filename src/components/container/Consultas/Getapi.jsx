9ae812e3c41825fad7a927309ff15c68a10a98e6
JuanmaTo <juanmatoniolo2@gmail.com>
2024-07-14 20:32:59 -0300

    cards de mascotas

---
+++ src/components/container/Consultas/Getapi.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+import { Button } from "react-bootstrap";
+import img from "../../../assets/img5.jpg";
+import "./get.css";
+
+function GetData({ id }) {
+	const [data, setData] = useState(null); // Estado para almacenar los datos obtenidos
+	const [codigosActivacion, setCodigosActivacion] = useState([]); // Estado para almacenar los códigos de activación
+
+	const url = `https://smartpet-1d59e-default-rtdb.firebaseio.com/usuario/${id}.json`;
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(url);
+				setData(response.data); // Actualiza el estado con los datos obtenidos
+
+				// Extrae y guarda los códigos de activación en un array
+				const codigos = Object.keys(response.data).reduce(
+					(acc, key) => {
+						if (response.data[key]?.codAct) {
+							acc.push(response.data[key].codAct);
+						}
+						return acc;
+					},
+					[]
+				);
+				setCodigosActivacion(codigos); // Actualiza el estado con los códigos de activación
+			} catch (error) {
+				console.error("Error al obtener datos:", error);
+			}
+		};
+
+		fetchData(); // Llama a la función fetchData al montar el componente y cuando 'id' cambia
+	}, [id, url]);
+
+	// Renderiza los datos obtenidos
+	return (
+		<div>
+			{data ? (
+				<div>
+					<section className="container contenedor-cards-user">
+						{Object.keys(data).map((key) =>
+							data[key]?.codAct ? (
+								<Card
+									className={data[key].codAct}
+									id={data[key].codAct}
+									key={key}
+									style={{
+										width: "17rem",
+										marginBottom: "20px",
+									}}
+								>
+									<Card.Img variant="top" src={img} className="img-card-pet"/>
+									<Card.Body>
+										<Card.Title className="titulo-card">
+											{data[key].codAct}
+										</Card.Title>
+										<Card.Text>
+											<p>Nombre: cargando...</p>
+											<p>Sexo: cargando...</p>
+											<p>Edad: cargando...</p>
+											{/* Asegúrate de usar el dato correcto según tu estructura */}
+											{/*   <p>DNI: {data[key].dni}</p> */}
+										</Card.Text>
+										<Button
+											variant="primary"
+											className="btn-editar"
+										>
+											Editar Mascotas{" "}
+										</Button>
+									</Card.Body>
+								</Card>
+							) : null
+						)}
+					</section>
+				</div>
+			) : (
+				<p>Cargando datos...</p>
+			)}
+		</div>
+	);
+}
+
+export default GetData;
--- src/components/container/Users/Consultas.jsx
+++ src/components/container/Users/Consultas.jsx
@@ -1,72 +1,17 @@
-import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
 import Logout from "../../Logout/Logout";
 import Footers from "../../footer/Footer";
 import SubscriptionForm from "../BodyConsultas";
 
+import Getapi from "../Consultas/Getapi";
+
 function Consultas() {
-	const { id } = useParams(); // Obtener el id de los parámetros de la URL
-	const [usuario, setUsuario] = useState(null); // Estado para almacenar los datos del usuario
-	const [codigosActivacion, setCodigosActivacion] = useState([]); // Estado para almacenar los códigos de activación únicos
-
-
-
-	useEffect(() => {
-		const fetchUsuario = async () => {
-			try {
-				const response = await axios.get(
-					`https://smartpet-1d59e-default-rtdb.firebaseio.com/usuario/${id}.json`
-				);
-				setUsuario(response.data); // Almacenar los datos del usuario en el estado
-			} catch (error) {
-				console.error("Error fetching user data:", error);
-			}
-		};
-
-		fetchUsuario(); // Llamar a la función para obtener los datos del usuario
-	}, [id]); // Ejecutar useEffect cada vez que cambie el id
-
-	useEffect(() => {
-		// Validar y almacenar códigos de activación únicos
-		if (usuario) {
-			const uniqueCodigos = [];
-			Object.keys(usuario).forEach((key) => {
-				if (key.startsWith("-")) {
-					const codAct = usuario[key].codAct;
-					if (!uniqueCodigos.includes(codAct)) {
-						uniqueCodigos.push(codAct);
-					}
-				}
-			});
-			setCodigosActivacion(uniqueCodigos);
-		}
-	}, [usuario]); // Ejecutar cada vez que cambien los datos del usuario
+	const { id } = useParams();
 
 	return (
 		<>
 			<Logout />
-
-			<div>
-				{usuario ? (
-					<div>
-						<h2>Detalles del Usuario</h2>
-						<p>Nombre: {usuario.nombreyapellido}</p>
-						<p>Usuario: {usuario.dni}</p>
-						<p>Correo Electrónico: {usuario.mail}</p>
-						<h3>Códigos de Activación:</h3>
-						<ul>
-							{codigosActivacion.map((codAct, index) => (
-								<li key={index}>Código: {codAct}</li>
-							))}
-						</ul>
-						{/* Mostrar otros detalles del usuario según tu estructura de datos */}
-					</div>
-				) : (
-					<p>Cargando usuario...</p>
-				)}
-			</div>
-
+			<Getapi id={id} />
 			<SubscriptionForm />
 			<Footers />
 		</>
