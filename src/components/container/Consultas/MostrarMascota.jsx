e5e97f5586c66a3c353cc9df092bf4e6fa2a88f0
JuanmaTo <juanmatoniolo2@gmail.com>
2024-08-25 20:54:47 -0300

    sex

--- src/components/container/Consultas/MostrarMascota.jsx
+++ src/components/container/Consultas/MostrarMascota.jsx
@@ -63,7 +63,7 @@ function MostrarMascota({ show, handleClose, mascota }) {
 									</p>
 									<p className="sexo-mascota">
 										{mascota.datosMascotas.sexo ===
-										"Hembra" ? (
+										"Macho" ? (
 											<svg
 												xmlns="http://www.w3.org/2000/svg"
 												width="18"
