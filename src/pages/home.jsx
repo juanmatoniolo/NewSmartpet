0437e190c6ba50ae9ebd56e39e2c7ca00810ec77
juanmatoniolo <juanmatoniolo2@gmail.com>
2025-01-31 16:49:03 -0300

    dominio tagsmartpet.com

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,19 +16,19 @@ function Homepage() {
 			<Header />
 			<Barnav />
 			<WhatsAppButton
-				mensaje={"Hola estoy interesado en adquirir un collar smartpet"}
+				mensaje={"Hola estoy interesado en adquirir un collar SmartPet"}
 			/>
 			<div>
 				<header className="header">
 					<h1 className="title">Bienvenido a SmartPet</h1>
 					<p className="subtitle">
-						Collares inteligentes para tus mascotas
+						Innovación en Identificación para tus Mascotas
 					</p>
 					<Button
 						variant="primary"
 						onClick={() => {
 							window.open(
-								"https://wa.me/+5493412275598?text=¡Hola! Estoy interesado en comprar un collar SmarPet!",
+								"https://wa.me/+5493412275598?text=¡Hola! Estoy interesado en comprar un collar SmartPet!",
 								"_blank"
 							);
 						}}
@@ -39,26 +39,27 @@ function Homepage() {
 				</header>
 
 				<section className="section section-body">
-					<Container>
+					<Container className="container-fluid
+					">
 						<Row>
-							<Col md={7} sm={12} lg={8}>
+							<Col md={7} sm={12} lg={8} className="columna-home">
 								<h2>¿Qué es SmartPet?</h2>
 								<p>
-									SmartPet es un colgante identificatorio para
-									mascotas que ofrece dos opciones para
-									acceder a la información de tu mascota.
-									Puedes escanear el código QR con la cámara
-									de tu teléfono o utilizar el chip
-									inteligente basado en NFC. Al acercar tu
-									teléfono al chip NFC, serás redirigido
-									automáticamente a una base de datos en línea
-									donde se encuentran los datos de la mascota.
-									De esta manera, cualquier persona puede
-									acceder a la información utilizando el QR o
-									el NFC.
+									SmartPet lleva la identificación de mascotas al siguiente nivel con la
+									digitalización de su información. A través de un colgante personalizado con
+									código QR y la opción de integrar un chip de aproximación para celulares
+									ínteligentes, cualquier persona puede acceder a los datos esenciales de la
+									mascota con un simple escaneo o acercando su teléfono. La base de datos
+									asociada permite a los dueños actualizar la información de su mascota
+									cuantas veces lo necesiten, asegurando que siempre esté al día. Además,
+									cuenta con una función que guarda la última ubicación donde se escaneó,
+									brindando una herramienta adicional en caso de extravío. Con botones de
+									contacto directo a llamadas, WhatsApp e Instagram, SmartPet facilita la
+									comunicación instantánea con los dueños, ayudando a reunir mascotas con sus
+									familias de manera rápida y eficiente.
 								</p>
 							</Col>
-							<Col md={5} sm={12} lg={4}>
+							<Col md={5} sm={12} lg={4} className="columna-home2">
 								<div className="container-carrusel">
 									<ControlledCarousel
 										a={Imagenes.imgs5}
@@ -78,14 +79,14 @@ function Homepage() {
 					</Container>
 				</section>
 
-				<section className=" container section section-body bg-light">
+				<section className="section section-body bg-light">
 					<Container>
 						<Row>
 							<Col
 								md={5}
 								sm={12}
 								lg={4}
-								className="d-none d-md-block"
+								className="d-none d-md-block columna-home2"
 							>
 								<img
 									src={Imagenes.imgs1}
@@ -93,41 +94,46 @@ function Homepage() {
 									className="img-fluid imgs-body"
 								/>
 							</Col>
-							<Col md={7} sm={12} lg={8}>
-								<h2>¿Cómo funciona SmartPet?</h2>
+							<Col md={7} sm={12} lg={8} className="columna-home">
+								<h2>¿Cómo Funciona SmartPet?</h2>
 								<p>
-									El usuario recibe el colgante con un código
-									de 4 letras y 3 números, único e
-									irrepetible. El usuario se registra en la
-									página de SmartPet y agrega el código para
-									poder configurar su mascota. La base de
-									datos contiene una imagen de la mascota,
-									nombre, sexo, ciudad, edad, descripción
-									(tratamientos importantes), y contactos de 2
-									personas con botones de acceso directo a
-									Instagram, llamada telefónica y WhatsApp.
+									Registro Único: Al adquirir tu colgante SmartPet, recibirás un código único de 4
+									letras y 3 números.
 								</p>
-								<p>Cuando alguien escanea el QR, recibirás una notificación con su ubicación* y se mostrarán las vías de contacto que hayas elegido. Esto permite un contacto directo con el dueño sin importar si ha perdido su teléfono, además de ofrecerte el beneficio agregado de recibir la ubicación de tu mascota en el momento en que alguien escanee su QR.
-
-*Recibirás una notificación que informa que alguien ha escaneado el QR de tu mascota y la ubicación aproximada. Si la persona que escanea el QR comparte permisos de ubicación, también obtendrás la ubicación precisa.</p>
+								<p>
+									Configuración Sencilla: Regístrate en nuestra página web y asocia el código a tu
+									cuenta para configurar el perfil de tu mascota.
+								</p>
+								<p>
+									Perfil Personalizado: Completa la información de tu mascota, incluyendo:
+									<ul>
+										<li>Foto</li>
+										<li>Nombre</li>
+										<li>Sexo</li>
+										<li>Ciudad</li>
+										<li>Edad</li>
+										<li>Descripción (tratamientos importantes, características especiales)</li>
+										<li>Contactos de hasta 2 personas, con accesos directos a Instagram, llamadas
+											telefónicas y WhatsApp.</li>
+									</ul>
+								</p>
+								<p>Actualización Continua: Puedes editar y actualizar la información de tu mascota en
+									cualquier momento, asegurando que siempre esté vigente.</p>
 							</Col>
 						</Row>
 					</Container>
 				</section>
 
-				<section className="section section-body ">
+				<section className="section section-body">
 					<Container>
 						<Row>
 							<Col md={7} sm={12} lg={8}>
-								<h2>Características Únicas</h2>
+								<h2>Características Destacadas</h2>
 								<p>
-									La digitalización de los datos no sensibles
-									y útiles de tu mascota con solo escanear un
-									QR o acercar el teléfono celular al collar.
-									Están hechos con resina epoxi, altamente
-									personalizables, y también ofrecemos
-									collares sublimados con diseños
-									personalizados.
+									La digitalización de los datos no sensibles y útiles de tu mascota con solo
+									escanear un QR o acercar el teléfono celular al collar. Están hechos con resina
+									epoxi, altamente personalizables, y también ofrecemos collares sublimados con
+									diseños personalizados.
 								</p>
 							</Col>
 							<Col md={5} sm={12} lg={4} className="esconder">
@@ -151,15 +157,12 @@ function Homepage() {
 									className="img-fluid imgs-body esconder"
 								/>
 							</Col>
-							<Col md={7} sm={12} lg={8} col={6}>
+							<Col md={7} sm={12} lg={8}>
 								<h2>Opciones de Personalización</h2>
 								<p>
-									Puedes escoger el color, tamaño, el uso o no
-									de chip NFC, y editar el contenido de la
-									base de datos de tu mascota. La producción
-									de un collar personalizado lleva de 3 a 5
-									días. Los collares sublimados se entregan en
-									1 a 2 días.
+									Puedes escoger el color, tamaño, el uso o no de chip NFC, y editar el contenido de
+									la base de datos de tu mascota. La producción de un collar personalizado lleva de
+									3 a 5 días. Los collares sublimados se entregan en 1 a 2 días.
 								</p>
 							</Col>
 						</Row>
