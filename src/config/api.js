3129f7d5b220fd81de22bb59035d765bdc249cc0
jwebchajari <jwebchajari@gmail.com>
2026-04-30 17:30:43 -0300

    api ruta

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ const envUrl = process.env.REACT_APP_API_URL;
 
 const fallbackUrl =
 	process.env.NODE_ENV === "production"
-		? "https://tagsmartpet.com/API-SMARTPET"
+		? "https://tagsmartpet.com/api-smartpet"
 		: "http://localhost/API-SMARTPET";
 
 const API_BASE = (envUrl || fallbackUrl).replace(/\/$/, "");
